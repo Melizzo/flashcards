2d006a70ee1f492177c29344e63c7cb7aa75ad1e
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-04-28 14:30:22 -0600

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+class Card {
+    constructor(id, question, answers = ['answer1', 'answer2', 'answer3'], correctAnswer) {
+        this.id = id,
+        this.question = question,
+        this.answers = answers,
+        this.correctAnswer = correctAnswer
+    }
+};
+
+module.exports = Card;
